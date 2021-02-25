f4bd819ea6f96766f9c4536ce0401913429b4c9b
MrSullivan <rsullivan@stcadocs.com>
2021-02-25 22:53:24 +0000

    if and elif statements.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    first_number = int(input("Give the first number:"))
+    second_number = int(input("Give the second number:"))
+
+    if(first_number>second_number):
+      print("The greater number is " + str(first_number))
+    elif(first_number<second_number):
+      print("The greater number is " + str(second_number))
+    elif(first_number==second_number):
+      print("The numbers are equal!")
 
 if __name__ == '__main__':
     main()
